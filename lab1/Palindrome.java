a6be5766f3599cddec3d0df5594c7b4d1f50fdd0
YoarashiYume <61472665+YoarashiYume@users.noreply.github.com>
2020-05-14 16:44:33 +0300

    Update Palindrome.java

--- lab1/Palindrome.java
+++ lab1/Palindrome.java
@@ -1,18 +1,19 @@
+import java.util.Scanner;
 public class Palindrome {
     private static boolean isPal(String input) {
         int leng = input.length() - 1;
         int midle = (leng / 2) + (leng % 2);
-        for (int i = 0; i < midle; i++) 
-            if (input.charAt(i) != input.charAt(leng - i)) { return false; }        
+        for (int i = 0; i < midle; i++)
+            if (input.charAt(i) != input.charAt(leng - i)) { return false; }
         return true;
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            if (isPal(args[i]))
-                System.out.print(args[i] + " is Palindrome\n");
-            else
-                System.out.print(args[i] + "Not Palindrome\n");
-        }
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        if (isPal(word))
+            System.out.print(word + " is Palindrome\n");
+        else
+            System.out.print(word + "Not Palindrome\n");
     }
 }
